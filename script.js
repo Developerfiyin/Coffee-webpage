288f93449b70d3882ffc66eb314e2ba9483fb267
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-02-12 23:44:42 +0100

    initialize Swiper in JS.

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ menuOpenButton.addEventListener("click", () => {
 
 //  close menu when the close button is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
